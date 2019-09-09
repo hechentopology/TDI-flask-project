55df1741e8361b5d0300f3038313b7c8bad8c3ac
housedragon <housedragon2018@gmail.com>
2019-09-08 21:01:28 -0700

    added bokeh server

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect
 from bokeh.plotting import figure, output_file, show, curdoc
-from bokeh.embed import components 
+from bokeh.embed import server_document
 from bokeh.core.properties import value 
 from bokeh.layouts import row, column,WidgetBox
 from bokeh.models import ColumnDataSource, CustomJS, HoverTool
 from bokeh.models.widgets import Slider, TextInput
 from bokeh.transform import dodge , jitter, factor_cmap
 from bokeh.models.markers import Asterisk,DiamondCross  
+from bokeh.server.server import Server
+from tornado.ioloop import IOLoop
+
 import quandl
 import pandas as pd 
 
@@ -14,14 +17,9 @@ import pandas as pd
 app = Flask(__name__)
 
 
-quandl.ApiConfig.api_key = "XHUMj4gG2AGsnwtxWkx6"
-feature_names = ['AAPL','MSFT','GOOG']
+quandl.ApiConfig.api_key = "XHUMj4gG2AGsnwtxWkx6" 
 
-
-@app.route('/')
-
-
-def create_figure(ticker_name):
+def modify_doc(doc):
   month_data = quandl.get(("WIKI/"+ticker_name.strip()), start_date="2005-12-01", end_date="2005-12-31") 
   x = month_data.index
   y = month_data['Close']
@@ -29,25 +27,37 @@ def create_figure(ticker_name):
 
   p1 = figure(title='one month stock AAPL',plot_height = 300, plot_width = 600) # , background_fill_color = '#efefef'
   r = p1.line(x="x", y="y", source=source, color = '#8888cc',line_width=1.5,alpha= 0.8)
-  p = column(p1,ticker)
-  return p
+
+  def callback(attr, old, new):
+
+      month_data = quandl.get(("WIKI/"+ new.strip()), start_date="2005-12-01", end_date="2005-12-31") 
+      x = month_data.index
+      y = month_data['Close']
+
+      source.data = ColumnDataSource(data=dict(x=x, y=y)).data
+
+  slider = TextInput(value="AAPL", title="Stock:")
+  slider.on_change('value', callback)
+
+  doc.add_root(column(slider, p1))
+ 
+@app.route('/', methods=['GET'])
+
+def bkapp_page():
+    script = server_document('http://localhost:5006/bkapp')
+    return render_template("index.html", script=script, template="Flask")
 
 
-def index(): 
+def bk_worker():
+    # Can't pass num_procs > 1 in this configuration. If you need to run multiple
+    # processes, see e.g. flask_gunicorn_embed.py
+    server = Server({'/bkapp': modify_doc}, io_loop=IOLoop(), allow_websocket_origin=["localhost:8000"])
+    server.start()
+    server.io_loop.start()
 
-  current_feature_name = request.args.get("feature_name")
-  if current_feature_name == None:
-    current_feature_name = "AAPL"
+from threading import Thread
+Thread(target=bk_worker).start()
 
-  #ticker = TextInput(value="AAPL", title="Stock:")
- # ticker.on_change('value', lambda attr, old, new: update())
-  
-  
-  p = create_figure(current_feature_name)
-
-  script, div = components(p)
-
-  return render_template('index.html',script=script, div=div,  feature_names=feature_names, current_feature_name=current_feature_name)
 
 @app.route('/about')
 def about():
