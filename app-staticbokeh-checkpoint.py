ce93bcb736a69aa07efa60ce7d256adef5ddf264
housedragon <housedragon2018@gmail.com>
2019-09-08 20:39:10 -0700

    f

--- app-staticbokeh-checkpoint.py
+++ app-staticbokeh-checkpoint.py
@@ -13,41 +13,41 @@ import pandas as pd
 
 app = Flask(__name__)
 
-
-quandl.ApiConfig.api_key = "XHUMj4gG2AGsnwtxWkx6"
-feature_names = ['AAPL','MSFT','GOOG']
-
-
 @app.route('/')
+def index(): 
 
+  quandl.ApiConfig.api_key = "XHUMj4gG2AGsnwtxWkx6"
+  month_data = quandl.get("WIKI/AAPL", start_date="2005-12-01", end_date="2005-12-31")
 
-def create_figure(ticker_name):
-  month_data = quandl.get(("WIKI/"+ticker_name.strip()), start_date="2005-12-01", end_date="2005-12-31") 
   x = month_data.index
   y = month_data['Close']
   source = ColumnDataSource(data=dict(x=x, y=y))
 
   p1 = figure(title='one month stock AAPL',plot_height = 300, plot_width = 600) # , background_fill_color = '#efefef'
   r = p1.line(x="x", y="y", source=source, color = '#8888cc',line_width=1.5,alpha= 0.8)
+ 
+  ticker = TextInput(value="AAPL", title="Stock:")
+
+  def update():
+    month_data = quandl.get(("WIKI/"+ticker.value.strip()), start_date="2005-12-01", end_date="2005-12-31") 
+    source.data = dict(
+        x=month_data.index,
+        y=month_data['Close'], 
+    )
+    show(p)
+
+  ticker.on_change('value', lambda attr, old, new: update())
+  
   p = column(p1,ticker)
-  return p
+  update()  # initial load of the data
 
+  #curdoc().add_root(p) 
 
-def index(): 
-
-  current_feature_name = request.args.get("feature_name")
-  if current_feature_name == None:
-    current_feature_name = "AAPL"
-
-  #ticker = TextInput(value="AAPL", title="Stock:")
- # ticker.on_change('value', lambda attr, old, new: update())
   
-  
-  p = create_figure(current_feature_name)
 
   script, div = components(p)
 
-  return render_template('index.html',script=script, div=div,  feature_names=feature_names, current_feature_name=current_feature_name)
+  return render_template('index.html',script=script, div=div)
 
 @app.route('/about')
 def about():
