a1fbfc44c0730c9937b8106398fbc8d6d60221c4
housedragon <housedragon2018@gmail.com>
2019-09-09 22:39:10 -0700

    f

--- app.py
+++ app.py
@@ -19,7 +19,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return redirect('/prices') 
+    return redirect('/index')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/prices', methods=['POST'])
 def prices():
